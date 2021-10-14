59339e699ec550abd7f382c812aaf7d96fd98d05
Yasuko Sasai <>
2021-10-14 23:07:12 +0900

    feat: syntac highlight

--- components/blog/index.js
+++ components/blog/index.js
@@ -1,6 +1,6 @@
 import styles from '../../styles/Home.module.scss';
 // pages/blog/[id].js
-export const Blog = ({ blog }) => {
+export const Blog = ({ blog, highlightedBody }) => {
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>{blog.title}</h1>
@@ -8,7 +8,8 @@ export const Blog = ({ blog }) => {
       <p className="category">{blog.category && `${blog.category.name}`}</p>
       <div
         dangerouslySetInnerHTML={{
-          __html: `${blog.body}`,
+          // __html: `${blog.body}`,
+          __html: `${highlightedBody}`,
         }}
         className={styles.post}
       />
--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,9 +1,13 @@
 import { Blog } from '../../components/blog';
 import { client } from '../../libs/client';
+import cheerio from 'cheerio';
+import hljs from 'highlight.js'
+import 'highlight.js/styles/night-owl.css';
+
 // pages/blog/[id].js
-export default function BlogId({ blog }) {
+export default function BlogId({ blog, highlightedBody }) {
   return (
-    <Blog blog={blog}/>
+    <Blog blog={blog} highlightedBody={highlightedBody}/>
   );
 }
 
@@ -19,10 +23,17 @@ export const getStaticPaths = async () => {
 export const getStaticProps = async (context) => {
   const id = context.params.id;
   const data = await client.get({ endpoint: "blog", contentId: id });
+  const $ = cheerio.load(data.body)
+  $('pre code').each((_, elm) => {
+    const result = hljs.highlightAuto($(elm).text())
+    $(elm).html(result.value)
+    $(elm).addClass('hljs')
+  })
 
   return {
     props: {
       blog: data,
+      highlightedBody: $.html(),
     },
   };
 };
